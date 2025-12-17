ea832f988eb4b2e110a895923afb512ab9c3cc84
DATA GURU <gyrogodnon@gmail.com>
2025-12-17 15:50:40 +0530

    commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ logger = get_logger(__name__)
 
 st.set_page_config(page_title="YT SEO Isnsights", layout="wide")
 
-st.title("YT SEO Insights Generator")
+st.title("AI YT SEO Insights Generator")
 st.write("Generate Tags , Audience Analysis , Timestamps AI Generated , Flaws Suggestions")
 
 with st.sidebar:
